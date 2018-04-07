9a27b69a742fa6d207a4b4fcf5d5f7bee5b677b9
Mike Rainey <bionic.oldsmobile@gmail.com>
2018-04-06 21:50:21 -0400

    changing nix script to build by default from the local repository

--- script/default.nix
+++ script/default.nix
@@ -1,17 +1,13 @@
 { pkgs   ? import <nixpkgs> {},
   stdenv ? pkgs.stdenv,
-  fetchurl,
+  chunkedseqSrc ? ../.,
   buildDocs ? false
 }:
 
 stdenv.mkDerivation rec {
-  name = "chunkedseq-${version}";
-  version = "0.1";
+  name = "chunkedseq";
 
-  src = fetchurl {
-    url = "https://github.com/deepsea-inria/chunkedseq/archive/v${version}.tar.gz";
-    sha256 = "f6bea1d30a0e66c2a1b9a65891ce4013b2c8a73fd33c981fe217d06a3f14dfba";
-  };
+  src = chunkedseqSrc;
 
   buildInputs = if buildDocs then [ pkgs.pandoc ] else [];
 
