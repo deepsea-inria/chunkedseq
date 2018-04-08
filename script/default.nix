d2925cf385fb43aff7eeb9c08cce88d321e5e02e
Mike Rainey <bionic.oldsmobile@gmail.com>
2018-04-08 13:31:43 -0400

    getting documentation to build in nix

--- script/default.nix
+++ script/default.nix
@@ -9,19 +9,31 @@ stdenv.mkDerivation rec {
 
   src = chunkedseqSrc;
 
-  buildInputs = if buildDocs then [ pkgs.pandoc ] else [];
+  buildInputs =
+    if buildDocs then
+      let pandocTypes = pkgs.haskellPackages.ghcWithPackages (pkgs: with pkgs; [pandoc-types]);
+      in
+      [ pkgs.pandoc pandocTypes pkgs.texlive.combined.scheme-small ]
+    else
+      [];
 
   buildPhase = if buildDocs then ''
-    make -C doc chunkedseq.pdf chunkedseq.html5
-  '' else
-    null;
+      make -C doc chunkedseq.pdf chunkedseq.html
+    '' else "";
 
-  installPhase = ''
+  installPhase =
+    let doc =
+      if buildDocs then ''
+        mkdir -p $out/doc/
+        cp doc/chunkedseq.pdf doc/chunkedseq.md doc/chunkedseq.html doc/chunkedseq.css $out/doc/
+      ''
+      else "";
+    in
+    ''
     mkdir -p $out/include/
     cp include/*.hpp $out/include
-    mkdir -p $out/doc/
-    cp doc/*.md doc/*.css doc/*.pdf doc/*.html5 doc/Makefile $out/doc/
-  '';
+    ${doc}
+    '';
 
   meta = {
     description = "A container data structure for representing sequences by many fixed-capacity heap-allocated buffers (i.e., chunks).";
